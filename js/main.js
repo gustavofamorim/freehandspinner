d1a3ab639a96b95531c2729196f77247085e3dee
Gustavo Freitas <gustavofreitasamorim@gmail.com>
2017-06-01 12:36:55 -0300

    Mobile improvements and using jqury touch punch to drag with touch support

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 
+$("#draggable").draggable({
+	containment:"body",
+	drag: function(event, ui){
+		$("#screen").css({
+			"left": ui.position.left,
+			"top": ui.position.top
+		});
+	}
+});
+
 $(window).ready(function(){
-
-	$("#draggable").draggable({
-		containment:"body",
-		start: function(){
-			console.log("start");
-		},
-		drag: function(event, ui){
-			$("#screen").css({
-				"left": ui.position.left - 114,
-				"top": ui.position.top + 187
-			});
-		}
-	});
-
-
 	$("#screen").propeller({
 		inertia: 0.99,
 		speed: 0
 	});
-
 });
